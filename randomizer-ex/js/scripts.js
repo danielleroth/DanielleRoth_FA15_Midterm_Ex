882609e788f5b6577aad100007681dae107ccadb
danielleroth <dgump62@it-mmc-m08.eastern.ewu.edu>
2015-11-09 23:00:01 -0800

    random

--- randomizer-ex/js/scripts.js
+++ randomizer-ex/js/scripts.js
@@ -6,35 +6,36 @@
 // eval() : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/eval
 
 // Generate random number between for three images
-var maxRand = 3;
-var numRand = Math.floor(Math.random() * maxRand + 1);
+
+    var maxRand = 3;                        
+    var numRand = Math.floor(Math.random() * maxRand + 1);
 
 console.log(numRand);
 
-eval(numRand = 3)
-
 // Create & Store 3 different greetings. You choose. 
-if( numRand = 1) {
-
+if (numRand === 1 ) {
+    $('h2').text('Aloha!');
+    $('body').addClass('bg1');
+}
+ 
+if (numRand === 2 ) {
+    $('h2').text('Bonjour!');
     $('body').addClass('bg0');
 }
 
-if( numRand = 2) {
-
-    $('body').addClass('bg1');
-}
-
-if( numRand = 3) {
-
+if (numRand === 3 ) {
+    $('h2').text('Hola!');
     $('body').addClass('bg2');
 }
 
 
-
 // On Page Load insert random greeting
+
 // NOTE. Wrap/pass the option with eval() method
+
 // use eval() to convert string to variable-object
 
 
 // On Page Load add random body class to <body> element
 // See CSS body class `bg0`, `bg1`, `bg2`
+
